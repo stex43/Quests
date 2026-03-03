3d823a19a9feeb47af53155234868c3366a28b99
stex43 <stainy@yandex.ru>
2026-03-03 16:24:31 +0100

    slight refactoring

--- backend/src/main.py
+++ backend/src/main.py
@@ -3,11 +3,10 @@ import uuid
 from fastapi import FastAPI, status
 from fastapi.middleware.cors import CORSMiddleware
 from fastapi.params import Depends
-from pydantic import BaseModel
 from sqlalchemy import select
 from sqlalchemy.orm import Session
 
-from src import models
+from src import models, schemas
 from src.database import engine, get_db
 from src.settings import settings
 
@@ -21,42 +20,13 @@ app.add_middleware(
 )
 
 
-class QuestCreate(BaseModel):
-    title: str
-    description: str
-    arc_id: uuid.UUID
-
-
-class Quest(BaseModel):
-    id: uuid.UUID
-    title: str
-    description: str
-    arc_id: uuid.UUID
-
-
-class ArcCreate(BaseModel):
-    # todo: max length
-    title: str
-
-
-class Arc(BaseModel):
-    id: uuid.UUID
-    title: str
-
-
-class ArcExtended(BaseModel):
-    id: uuid.UUID
-    title: str
-    quests: list[Quest] = []
-
-
 @app.get("/")
 def read_root():
     return {"Hello": "World"}
 
 
-@app.post("/arcs", status_code=status.HTTP_201_CREATED, response_model=Arc)
-def create_arc(arc: ArcCreate, db_session: Session = Depends(get_db)):
+@app.post("/arcs", status_code=status.HTTP_201_CREATED, response_model=schemas.Arc)
+def create_arc(arc: schemas.ArcCreate, db_session: Session = Depends(get_db)):
     new_arc = models.Arc(id=uuid.uuid4(), title=arc.title)
     db_session.add(new_arc)
     db_session.commit()
@@ -64,20 +34,14 @@ def create_arc(arc: ArcCreate, db_session: Session = Depends(get_db)):
 
 
 # todo: paging
-@app.get("/arcs", status_code=status.HTTP_200_OK, response_model=list[ArcExtended])
+@app.get("/arcs", status_code=status.HTTP_200_OK, response_model=list[schemas.ArcExtended])
 def get_arcs(db_session: Session = Depends(get_db)):
     return db_session.scalars(select(models.Arc)).all()
 
 
-@app.post("/quests", status_code=status.HTTP_201_CREATED, response_model=Quest)
-def create_quest(quest: QuestCreate, db_session: Session = Depends(get_db)):
+@app.post("/quests", status_code=status.HTTP_201_CREATED, response_model=schemas.Quest)
+def create_quest(quest: schemas.QuestCreate, db_session: Session = Depends(get_db)):
     new_quest = models.Quest(id=uuid.uuid4(), title=quest.title, description=quest.description, arc_id=quest.arc_id)
     db_session.add(new_quest)
     db_session.commit()
     return new_quest
-
-
-# todo: paging
-@app.get("/quests", status_code=status.HTTP_200_OK, response_model=list[Quest])
-def get_quests(db_session: Session = Depends(get_db)):
-    return db_session.scalars(select(models.Quest)).all()
---
+++ backend/src/schemas.py
@@ -0,0 +1,32 @@
+import uuid
+
+from pydantic import BaseModel
+
+
+class QuestCreate(BaseModel):
+    title: str
+    description: str
+    arc_id: uuid.UUID
+
+
+class Quest(BaseModel):
+    id: uuid.UUID
+    title: str
+    description: str
+    arc_id: uuid.UUID
+
+
+class ArcCreate(BaseModel):
+    # todo: max length
+    title: str
+
+
+class Arc(BaseModel):
+    id: uuid.UUID
+    title: str
+
+
+class ArcExtended(BaseModel):
+    id: uuid.UUID
+    title: str
+    quests: list[Quest] = []
